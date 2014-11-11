588b5e69f4d039751d38252a840506b371262d54
kernel64 <aymen.kernel@gmail.com>
2014-11-11 23:12:17 +0100

    adding controller to render png files

---
+++ Controller/PngController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Mabs\BarCodeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class PngController extends Controller
+{
+    public function generateAction($type)
+    {
+        $request = $this->container->get('request_stack')->getCurrentRequest();
+        $data = $request->request->get('data',$request->get('data', null));
+        $content = $this->get('mabs_bar_code.service')->getPngBarCode($data, $type);
+
+        $response = new Response($content);
+        $response->headers->set('Content-Type', 'image/png');
+        return $response;
+    }
+}
